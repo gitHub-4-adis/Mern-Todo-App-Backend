4c2bc3c6f5e49d0b402881b5caa24a73fd2d431d
adis <temp.dev.00001@gmail.com>
2024-05-23 02:50:54 +0530

    configured cors

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 const app = express()
 
 import connect from './api/db/connect.js';
 import router from './api/routes/apiRoutes.js';
 
+app.use(cors({
+    origin: ["https://664e5d434e05c13919c92868--stellular-tiramisu-4e3688.netlify.app/", "https://664e5d434e05c13919c92868--stellular-tiramisu-4e3688.netlify.app/*"]
+}));
 app.use(express.json());
 
 app.use('/api', router);
