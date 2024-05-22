1966b0a951c4cad7637a66584ecc1cae784c9f7b
adis <temp.dev.00001@gmail.com>
2024-05-23 03:00:53 +0530

    configured cors3

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import connect from './api/db/connect.js';
 import router from './api/routes/apiRoutes.js';
 
 app.use(cors({
-    origin: "https://664e5d434e05c13919c92868--stellular-tiramisu-4e3688.netlify.app/"
+    origin: "https://664e5d434e05c13919c92868--stellular-tiramisu-4e3688.netlify.app/",
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 app.use(express.json());
 
