5f372a5f16399f751dc3e02050d933481255c27d
adis <temp.dev.00001@gmail.com>
2024-05-23 23:37:46 +0530

    dist folder

--- App.js
+++ App.js
@@ -8,12 +8,8 @@ const app = express()
 import connect from './api/db/connect.js';
 import router from './api/routes/apiRoutes.js';
 
-app.use(cors({
-    origin: "https://664e5d434e05c13919c92868--stellular-tiramisu-4e3688.netlify.app/",
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}));
+app.use(express.static('./dist'));
 app.use(express.json());
-
 app.use('/api', router);
 app.use((req, res) => {
     res.status(404).send("Error: 404 not found");
